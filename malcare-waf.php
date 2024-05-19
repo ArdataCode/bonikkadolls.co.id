2b01ea085c075ebf2f4e3fe2f200d82dbad65847
ArdataCode <ardata.indonesia@gmail.com>
2024-05-19 08:40:42 +0000

    mmita

---
+++ malcare-waf.php
@@ -0,0 +1,9 @@
+<?php
+// Please validate auto_prepend_file setting before removing this file
+
+if (file_exists('/home/jawa2439/public_html/bonikkadolls.co.id/wp-content/plugins/malcare-security/protect/prepend/ignitor.php')) {
+	define("MCDATAPATH", '/home/jawa2439/public_html/bonikkadolls.co.id/wp-content/mc_data/');
+	define("MCCONFKEY", '28472189dbcfebec0963a148bbcf0249');
+	include_once('/home/jawa2439/public_html/bonikkadolls.co.id/wp-content/plugins/malcare-security/protect/prepend/ignitor.php');
+}
+?>
---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"-^Y4rL /Schx(4e_U_o(8nM(2LZP=></>VxF{#2Bu}%J$9Y8Kr!B3irm)U?# OL-";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:1;s:13:"attackDataKey";i:3139;}
